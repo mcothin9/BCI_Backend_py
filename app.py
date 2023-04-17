37404de24d6acbe4c0f32000c4cc29b179467795
Mitchell <mingchenothin9@gmail.com>
2023-04-17 20:53:47 +1000

    feat: get update

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+# server.py
+from flask import Flask, jsonify
+from test_result_server import predict_eeg_events
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/probs', methods=['GET'])
+def get_probs():
+    probs = predict_eeg_events()
+    # Convert the numpy array to a Python list and wrap it in a dictionary
+    probs_list = probs.tolist()
+    response = {'probs': probs_list}
+    # print(response)
+    return jsonify(response)
 
 
-if __name__ == "__main__":
-    app.run("localhost", 6969)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test_result_server.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+import json
+import numpy as np
+import mne
+from mne import io
+from mne.datasets import sample
+import tensorflow as tf
+from tensorflow.keras import utils as np_utils
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import model_from_json
+import matplotlib.pyplot as plt
+
+def predict_eeg_events():
+    import os
+    import mne
+    import numpy as np
+    from tensorflow.keras.models import model_from_json
+    from tensorflow.keras import backend as K
+
+    json_file_path = os.path.expanduser('~/Desktop/Workspace/BCI/model_EEGNet.json')
+    weights_file_path = os.path.expanduser('~/Desktop/Workspace/BCI/model_EEGNet.h5')
+    data_file_path = os.path.expanduser('~/Desktop/Workspace/BCI/sample_audvis_filt-0-40_raw.fif')
+
+    with open(json_file_path, 'r') as json_file:
+        loaded_model_json = json_file.read()
+
+    model = model_from_json(loaded_model_json)
+    model.load_weights(weights_file_path)
+
+    K.set_image_data_format('channels_last')
+
+    raw = mne.io.read_raw_fif(data_file_path, preload=True)
+    raw.filter(2, None, method='iir')
+    events = mne.find_events(raw)
+    raw.info['bads'] = ['MEG 2443']  # set bad channels
+    picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False,
+                           exclude='bads')
+    epochs = mne.Epochs(raw, events, event_id={'audio left': 1, 'audio right': 2, 'visual left': 3, 'visual right': 4},
+                        tmin=-0., tmax=1.0, proj=False, picks=picks, baseline=None, detrend=None, preload=True)
+    preprocessed_data = epochs.get_data() * 1000
+
+    # make prediction with model
+    probs = model.predict(preprocessed_data)
+    preds = probs.argmax(axis=-1)
+
+    return probs
