9d9c1eb0be7d64f2594f966ebf4223289d5ba723
Mitchell <mingchenothin9@gmail.com>
2023-05-26 15:47:07 +1000

    build: delete unused file

--- test_bsl.py
+++
@@ -1,7 +0,0 @@
-import bsl
-from bsl import StreamPlayer
-
-if __name__ == '__main__':
-    dataset = bsl.datasets.eeg_resting_state.data_path()
-    player = StreamPlayer('StreamPlayer', dataset)
-    player.start()
